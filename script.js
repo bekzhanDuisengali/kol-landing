9b2a13a559b947304acab30c856632c498466474
bekzhanduisengali <beckzhan04@gmail.com>
2025-08-18 12:14:52 +0500

    Описание изменений

--- script.js
+++ script.js
@@ -24,3 +24,10 @@ window.addEventListener("DOMContentLoaded", () => {
     }, 1200);
   });
 });
+const io = new IntersectionObserver((entries)=>{
+  entries.forEach(e=>{
+    if(e.isIntersecting){ e.target.classList.add('visible'); io.unobserve(e.target); }
+  });
+},{threshold: 0.12});
+
+document.querySelectorAll('.reveal').forEach(el=>io.observe(el));
